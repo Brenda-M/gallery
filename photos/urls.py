b07e7d8a3be1597cc5a83c45879a65559cf42687
Brenda-M <brendanjuguna1@gmail.com>
2020-06-04 23:04:26 +0300

    add categories path

--- photos/urls.py
+++ photos/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='homepage')
-  path('/details/<int:pk>/', views.details, name='image_details')
+  path('', views.home, name='homepage'),
+  path('/categories/<str:cat_name>/', views.c)
+  # path('/details/<int:pk>/', views.details, name='image_details'),
 ]
