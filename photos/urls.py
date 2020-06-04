63fb23a29cb079e0aea5ca82316e506c05a5ebda
Brenda-M <brendanjuguna1@gmail.com>
2020-06-05 00:53:16 +0300

    create path for location list

--- photos/urls.py
+++ photos/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='homepage'),
-  path('/categories/<str:cat_name>/', views.c)
+  path('categories/', views.categories, name='category-list'),
+  path('locations/', views.categories, name='location-list'),
+  path('category/images/<int:category_name>', views.category_view, name='by_category'),
+  path('location/images/<int:place>', views.location_view, name='by_location'),
   # path('/details/<int:pk>/', views.details, name='image_details'),
 ]
