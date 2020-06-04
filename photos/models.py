c962a471c2c3a52b9ed0c39c3de6da6d2f9888aa
Brenda-M <brendanjuguna1@gmail.com>
2020-06-04 18:21:04 +0300

    create category class

--- photos/models.py
+++ photos/models.py
@@ -13,6 +13,14 @@ class Image(models.Model):
 class Location(models.Model):
   locale = models.CharField(max_length=20)
 
+  def __str__(self):
+    return self.locale
+
+class Category(models.Model):
+  cat_name = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.cat_name
 
 
 
