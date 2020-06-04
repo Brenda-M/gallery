93044a83b8581ddce208a3158068f48e04df8751
Brenda-M <brendanjuguna1@gmail.com>
2020-06-04 19:14:14 +0300

    configure static files storage

--- photos/models.py
+++ photos/models.py
@@ -1,15 +1,5 @@
 from django.db import models
 
-class Image(models.Model):
-  photo = models.ImageField(upload_to = 'projects/')
-  img_name = models.CharField(max_length=100, blank=True)
-  description = models.CharField(max_length=150)
-  location = models.ForeignKey(Location, on_delete=models.CASCADE)
-  category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-  def __str__(self):
-    return self.img_name
-
 class Location(models.Model):
   locale = models.CharField(max_length=20)
 
@@ -21,6 +11,18 @@ class Category(models.Model):
 
   def __str__(self):
     return self.cat_name
+
+
+class Image(models.Model):
+  photo = models.ImageField(upload_to = 'projects/')
+  img_name = models.CharField(max_length=100, blank=True)
+  description = models.CharField(max_length=150)
+  location = models.ForeignKey(Location, on_delete=models.CASCADE)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.img_name
+
 
 
 
