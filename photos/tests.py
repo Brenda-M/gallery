5b15bd86277723bf1dd1bc6a345f0242f499f401
Brenda-M <brendanjuguna1@gmail.com>
2020-06-04 20:17:22 +0300

    create templates folder

--- photos/tests.py
+++ photos/tests.py
@@ -5,10 +5,12 @@ class ImageTestClass(TestCase):
 
   def setUp(self):
     self.category = Category(cat_name='food')
-    self.location = Location(city='nairobi', country='kenya', locale='westlands')
-    self.image = Image(photo='test.jpg', img_name='test trip', description='short trip to jamaica', location=self.location, category=self.category)
-    self.location.save()
     self.category.save()
+
+    self.location = Location(city='nairobi', country='kenya', locale='westlands')
+    self.location.save()
+
+    self.image = Image(photo='test.jpg', img_name='test trip', description='short trip to jamaica', location=self.location, category=self.category)
     self.image.save()
   
   def test_instance(self):
@@ -16,3 +18,8 @@ class ImageTestClass(TestCase):
 
   def test_save(self):
     self.image.save_image()
+  
+  # def test_delete_image(self):
+  #   del_img = Image.object.filter(id=2).delete()
+
+   
