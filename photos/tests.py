906414b24590eef897f4d49990b5b9c9734afcb1
Brenda-M <brendanjuguna1@gmail.com>
2020-06-04 20:40:45 +0300

    add basic html boiler template

--- photos/tests.py
+++ photos/tests.py
@@ -10,16 +10,19 @@ class ImageTestClass(TestCase):
     self.location = Location(city='nairobi', country='kenya', locale='westlands')
     self.location.save()
 
-    self.image = Image(photo='test.jpg', img_name='test trip', description='short trip to jamaica', location=self.location, category=self.category)
-    self.image.save()
+    self.image1 = Image(photo='test.jpg', img_name='test trip', description='short trip to jamaica', location=self.location, category=self.category)
+    self.image1.save()
   
   def test_instance(self):
-    self.assertTrue(isinstance(self.image, Image))
+    self.assertTrue(isinstance(self.image1, Image))
 
   def test_save(self):
     self.image.save_image()
   
-  # def test_delete_image(self):
-  #   del_img = Image.object.filter(id=2).delete()
+  def test_delete_image(self):
+    self.image2 = Image(photo='test_2.jpg', img_name='test 2 trip', description='short trip to hawaii', location=self.location, category=self.category)
+    self.image.save()
+    img_del = Image.objects.get(photo='test_2.jpg')
+   
 
    
