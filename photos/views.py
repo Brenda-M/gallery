dd3e17fc71122fcfc6c08482cd94b3482df5dcf7
Brenda-M <brendanjuguna1@gmail.com>
2020-06-04 22:35:04 +0300

    create details view function

--- photos/views.py
+++ photos/views.py
@@ -4,10 +4,13 @@ from .models import Image, Location, Category
 
 def home(request):
 
-  all_images = Images.objects.all()
+  all_images = Image.objects.all()
 
   context = {
     'images':all_images
   }
 
   return render(request, 'photos/home.html', context)
+
+def details(request):
+  return render(request, 'photos/details.html')
