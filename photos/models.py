37cd5727f00d49c67cb540ee292e79a77c93dc12
Brenda-M <brendanjuguna1@gmail.com>
2020-06-06 14:02:25 +0300

    add cloudinary configurations

--- photos/models.py
+++ photos/models.py
@@ -17,7 +17,7 @@ class Category(models.Model):
     return self.cat_name
 
 class Image(models.Model):
-  photo = models.ImageField(upload_to = 'projects/')
+  photo = CloudinaryField('image')
   img_name = models.CharField(max_length=100, blank=True)
   description = models.CharField(max_length=150)
   location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='img_location')
